e77213d7e1c8087c5984bc010f11404d34304ded
Erdem <72281877+merseh@users.noreply.github.com>
2023-03-06 05:36:57 +0300

    Create minecraft.js

---
+++ minecraft.js
@@ -0,0 +1,35 @@
+const Discord = require('discord.js');
+const mineflayer = require('mineflayer');
+
+const client = new Discord.Client();
+const bot = mineflayer.createBot({
+  host: 'mc.example.com', // IP address of the Minecraft server
+  port: 25565, // Port
+  username: 'botname' // The username the bot will use in Minecraft
+});
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', message => {
+  if (message.content === '!afk') {
+    bot.chat('I am now AFK!');
+    bot.setControlState('forward', true);
+    bot.setControlState('sneak', true);
+  }
+});
+
+bot.on('spawn', () => {
+  console.log('Minecraft bot spawned!');
+});
+
+bot.on('chat', (username, message) => {
+  if (username !== bot.username && message === 'back') {
+    bot.setControlState('forward', false);
+    bot.setControlState('sneak', false);
+    bot.chat('I am no longer AFK!');
+  }
+});
+
+client.login('token');
